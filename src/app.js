03e48bd3960afdf4d7a81f16faa50c1758b28336
Manishkushwahh <manishkushwah169@gmail.com>
2025-02-15 14:50:17 +0530

    explore get,post,delete

--- src/app.js
+++ src/app.js
@@ -1,13 +1,23 @@
 const express = require("express");
 const app = express();
 
+app.get("/user", (req,res) => {
+    res.send({firstname:"Manish", lastname:"kushwah"});
+});
+
+app.post("/user", (req,res) => {
+    res.send("Data is added to database succussfully");
+});
+
+app.delete("/user", (req,res) => {
+    res.send("Data Deleted");
+});
+
 app.use("/test", (req,res) => {
     res.send("test test test test");
 });
 
-app.use("/hello", (req,res) => {
-    res.send("hello hello hello hello");
-});
+
 
 app.listen(3000, ()=> {
     console.log("Server is succussfully listening on port 3000...");
