f8ff942ca0ba399f1e634c82431e097ad0d0ab1a
Manishkushwahh <manishkushwah169@gmail.com>
2025-02-15 15:14:02 +0530

    play with routing use of params

--- src/app.js
+++ src/app.js
@@ -1,21 +1,12 @@
 const express = require("express");
 const app = express();
 
-app.get("/user", (req,res) => {
+app.get("/user/:userId/:name/:password", (req,res) => {
+    console.log(req.params);
     res.send({firstname:"Manish", lastname:"kushwah"});
 });
 
-app.post("/user", (req,res) => {
-    res.send("Data is added to database succussfully");
-});
 
-app.delete("/user", (req,res) => {
-    res.send("Data Deleted");
-});
-
-app.use("/test", (req,res) => {
-    res.send("test test test test");
-});
 
 
 
