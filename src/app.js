0b656f5eb8ab9474fd85c2be40a6c0932cc62c92
Manishkushwahh <manishkushwah169@gmail.com>
2025-04-03 21:05:46 +0530

    added chat feature using socket.io

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ const connectDB = require("./config/database");
 const app = express();
 const cookieParser = require("cookie-parser");
 const cors = require("cors");
+const http = require("http");
 
 require("dotenv").config();
 
@@ -22,21 +23,25 @@ const authRouter = require("./routes/authRouter");
 const profileRouter = require("./routes/profile");
 const requestRouter = require("./routes/request");
 const userRouter = require("./routes/user");
+const initialzeSocket = require("./utils/socket");
+const chatRouter = require("./routes/chat");
 
 app.use("/", authRouter);
 app.use("/", profileRouter);
 app.use("/", requestRouter);
 app.use("/", userRouter);
+app.use("/", chatRouter);
 
-
+const server = http.createServer(app);
+initialzeSocket(server);
 
 connectDB()
     .then(() => {
         console.log("Successfully connected to Database");
-        app.listen(process.env.PORT , () => {
+        server.listen(process.env.PORT , () => {
             console.log("Server is successfully listening on port 7777...");
         });
     })
     .catch((error) => {
-        console.error("Cannot connected to Database");
+        console.error("Cannot connected to Database" + error);
     });
